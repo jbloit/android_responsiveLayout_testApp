2b8ae52b1adf9a682803928f9f685385de7ba618
Julien Bloit <julien.bloit@gmail.com>
2018-06-12 09:41:56 +0200

    Step-slider sets images visibility.

--- app/src/main/java/com/jbloit/reactivelayouttestapp/FullscreenActivity.kt
+++ app/src/main/java/com/jbloit/reactivelayouttestapp/FullscreenActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.os.Handler
 import android.util.Log
 import android.view.View
+import android.widget.ImageView
 import android.widget.SeekBar
 import kotlinx.android.synthetic.main.activity_fullscreen.*
 
@@ -15,18 +16,41 @@ import kotlinx.android.synthetic.main.activity_fullscreen.*
 class FullscreenActivity : AppCompatActivity(), SeekBar.OnSeekBarChangeListener {
 
     val TAG = "FULLSCREEN"
+
+    private var imageViews: ArrayList<ImageView> = ArrayList()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_fullscreen)
 
         seekBar_verticalMargin.setOnSeekBarChangeListener(this)
+        seekBar_imageCount.setOnSeekBarChangeListener(this)
+
+        imageViews.add(img1)
+        imageViews.add(img2)
+        imageViews.add(img3)
+        imageViews.add(img4)
+
 
     }
 
     override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
 
-        guideline_margin1.setGuidelinePercent(progress.toFloat() / 100)
+        when (seekBar) {
+            seekBar_verticalMargin -> {
+                guideline_margin1.setGuidelinePercent(progress.toFloat() / 100)
+            }
+            seekBar_imageCount -> {
+                for (i in 0..3){
+                    imageViews[i].visibility = ImageView.GONE
+                }
+                for (i in 1..progress){
+                    Log.d(TAG, "progress : $i")
+                    imageViews[i-1]?.visibility = ImageView.VISIBLE
+                }
+            }
+        }
     }
 
     override fun onStartTrackingTouch(seekBar: SeekBar?) {
